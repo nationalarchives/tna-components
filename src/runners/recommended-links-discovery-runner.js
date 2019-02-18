7db49f46ea0c579a039734b1ff9e2daa38c4879d
NetDesignr <michaiel.webdesigner@gmail.com>
2019-02-18 14:18:42 +0000

    ReactDOM dender conditional based on the URL

--- src/runners/recommended-links-discovery-runner.js
+++ src/runners/recommended-links-discovery-runner.js
@@ -6,19 +6,23 @@ import RecommendedLinks from '../../components/recommended_links/recommended_lin
 // Define the block scope
 
 {
-  const wrapper = document.querySelector('.search-results .generic-tabs');
+  const wrapper = document.querySelector('.search-results .generic-tabs'),
+    url = window.location.href,
+    regex = /\?_q=/g;
 
-  if (!document.getElementById('recommended-links-discovery')) {
-    let div = document.createElement('div');
-    div.id = 'recommended-links-discovery';
+  if (url.match(regex) && wrapper) {
+    if (!document.getElementById('recommended-links-discovery')) {
+      let div = document.createElement('div');
+      div.id = 'recommended-links-discovery';
 
-    if (wrapper) {
-      wrapper.appendChild(div);
+      if (wrapper) {
+        wrapper.appendChild(div);
+      }
+
+      ReactDOM.render(
+        <RecommendedLinks />,
+        document.getElementById('recommended-links-discovery')
+      );
     }
-
-    ReactDOM.render(
-      <RecommendedLinks />,
-      document.getElementById('recommended-links-discovery')
-    );
   }
 }
