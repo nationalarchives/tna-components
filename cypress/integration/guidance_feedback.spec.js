e5c45bd9e3fbdac86353fb3165c7752049d4ae40
NetDesignr <michaiel.webdesigner@gmail.com>
2019-02-27 15:21:12 +0000

    Add E2E tests on Recommended links

--- cypress/integration/guidance_feedback.spec.js
+++ cypress/integration/guidance_feedback.spec.js
@@ -44,5 +44,35 @@ describe('Eehehehhe', () => {
       'have.text',
       'Did you find the guidance you needed?'
     );
+    cy.get('.btnGF--no.show')
+      .should('be.visible')
+      .click()
+      .should('be.hidden');
+    cy.get('#guidance-feedback-react form#guidance_feedback label').should(
+      'have.text',
+      'What did you expect to find?'
+    );
+    cy.get('textarea#field-no')
+      .should('be.visible')
+      .type('Cypress end-to-end testing', { delay: 100 });
+    cy.wait(3000);
+    cy.get('.btnGF--cancel').click();
+    cy.wait(3000);
+    cy.get('#guidance-feedback-react')
+      .scrollIntoView()
+      .should('be.visible');
+    cy.get('.btnGF--yes.show')
+      .should('be.visible')
+      .click()
+      .should('be.hidden');
+    cy.wait(3000);
+    cy.get('textarea#field-yes')
+      .should('be.visible')
+      .type('Cypress end-to-end testing', { delay: 100 });
+    cy.get('.btnGF--send').click();
+    cy.get('#guidance-feedback-react form#guidance_feedback h3#aria').should(
+      'have.text',
+      'Thank you for your feedback.'
+    );
   });
 });
