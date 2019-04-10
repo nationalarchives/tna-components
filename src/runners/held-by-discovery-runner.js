4b52f8a9874f3b346498b5bd6cd01c4ca7c40ba9
NetDesignr <michaiel.webdesigner@gmail.com>
2019-04-10 17:47:25 +0100

    Rename the main component, add more child components

--- src/runners/held-by-discovery-runner.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import HeldBy from '../../components/held_by/heldBy.react';
-
-ReactDOM.render(<HeldBy />, document.querySelector('.field-container'));
