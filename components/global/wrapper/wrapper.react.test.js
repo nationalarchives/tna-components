69aac1d34c07788546bcd3175602683dea17ac20
NetDesignr <michaiel.webdesigner@gmail.com>
2019-04-18 17:44:07 +0100

    Create global components, add form validation, unit/snapshot tests

--- components/global/wrapper/wrapper.react.test.js
+++ components/global/wrapper/wrapper.react.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
 import Wrapper from './wrapper.react';
-import Headline from './headline.react';
+import Headline from '../headline/headline.react';
 
 let testRenderer, testInstance, testRendererJSON;
 
@@ -17,10 +17,6 @@ beforeAll(() => {
 
 describe('Wrapper component', () => {
   it('=> Prop ID = held-by-react-component', () => {
-    expect(testRendererJSON.props.id).toBe('held-by-react-component');
-  });
-
-  it("=> Headline's tag type h1 exists", () => {
-    expect(testRendererJSON.children[0].type).toBe('h1');
+    expect(testInstance.type(Wrapper).props.id).toBe(undefined);
   });
 });
