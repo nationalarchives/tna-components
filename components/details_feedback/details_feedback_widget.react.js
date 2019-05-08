9d77c5062c41f31f2e1c55a6db7418c4e286959d
Punal Chotrani <punalsc@gmail.com>
2019-05-08 19:05:36 +0100

    Environment setup

--- components/details_feedback/details_feedback_widget.react.js
+++ components/details_feedback/details_feedback_widget.react.js
@@ -1,1 +1,14 @@
-console.log("React component");
+import React, { Component } from "react";
+
+import "./style.scss";
+
+class DetailsFeedbackWidget extends Component {
+	render() {
+		return (
+			<div className="details-feedback-widget">
+				<h3>Hello</h3>
+			</div>
+		);
+	}
+}
+export default DetailsFeedbackWidget;
