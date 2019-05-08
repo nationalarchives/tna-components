0d1db08def74e7c5c32b03f1b6cf98bab708dcce
Punal Chotrani <punalsc@gmail.com>
2019-05-08 19:05:36 +0100

    Environment setup

--- src/runners/details-feedback.js
+++ src/runners/details-feedback.js
@@ -1,3 +1,8 @@
-{
-	console.log("Hello");
-}
+import React from "react";
+import ReactDOM from "react-dom";
+import DetailsFeedbackWidget from "../../components/details_feedback/details_feedback_widget.react";
+
+ReactDOM.render(
+	<DetailsFeedbackWidget />,
+	document.getElementById("details-feedback-wrapper")
+);
