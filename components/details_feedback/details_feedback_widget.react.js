a5ba2f73f5e993402d466a430cf2c19cb48d695d
Punal Chotrani <punalsc@gmail.com>
2019-05-10 18:21:48 +0100

    Updated header component

--- components/details_feedback/details_feedback_widget.react.js
+++ components/details_feedback/details_feedback_widget.react.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import DetailsFeedbackForm from "./child_components/form/details_feedback_widget.react";
+import FeedbackHeader from "./child_components/header/details_feedback_header.react";
 
 import "./style.scss";
 
@@ -8,7 +9,7 @@ class DetailsFeedbackWidget extends Component {
 	render() {
 		return (
 			<div className="details-feedback-widget">
-				<h2>Feedback</h2>
+				<FeedbackHeader componentTitle="Feedback" />
 				<DetailsFeedbackForm />
 			</div>
 		);
