2e8333c4751533a1ef8cb876f8b1af0335573db0
Punal Chotrani <punalsc@gmail.com>
2019-05-13 10:22:38 +0100

    Scoping styles to the component

--- components/details_feedback/child_components/header/details_feedback_header.react.js
+++ components/details_feedback/child_components/header/details_feedback_header.react.js
@@ -2,9 +2,17 @@ import React from "react";
 
 const FeedbackHeader = props => {
 	const { componentTitle } = props;
+	const h2Style = {
+		fontFamily: "'Open Sans',sans-serif",
+		fontSize: "1em",
+		fontWeight: 700,
+		display: "inline - block",
+		margin: ".3em 0",
+		color: "#000"
+	};
 	return (
 		<React.Fragment>
-			<h2>{componentTitle}</h2>
+			<h2 style={h2Style}>{componentTitle}</h2>
 		</React.Fragment>
 	);
 };
