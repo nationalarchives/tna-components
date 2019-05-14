b6a96140715c8161cc2e0eee1ff877d5cf5a8188
Punal Chotrani <punalsc@gmail.com>
2019-05-14 15:31:49 +0100

    Added fieldsets with state

--- components/details_feedback/child_components/form/details_feedback_form.react.js
+++ components/details_feedback/child_components/form/details_feedback_form.react.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
-import Fieldset from '../form/details_feedback_fieldset.react';
+import FieldsetWrapper from './fieldset/details_feedback_fieldset.react';
+import FeedbackButton from './button/details_feedback_widget_button.react';
 
 class DetailsFeedbackForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      displayFieldSet: false
+      noFieldsetDisplay: false,
+      yesFieldsetDisplay: false
     };
   }
   render() {
     return (
-      <React.Fragment>
-        <form action="" id="discovery-feedback-form">
-          <Fieldset />
-        </form>
-      </React.Fragment>
+      <form action="">
+        <FieldsetWrapper>
+          <legend>Could this page be improved</legend>
+          <FeedbackButton buttonText="No" />
+          <FeedbackButton buttonText="Yes" />
+        </FieldsetWrapper>
+
+        {this.state.noFieldsetDisplay && (
+          <FieldsetWrapper ariaExpanded={this.state.noFieldsetDisplay}>
+            <legend>We'd like to hear from you</legend>
+            <div class="checkbox">
+              <label for="explore-comments">
+                Your feedback helps us improve our services. Please share any
+                comments below.
+              </label>
+              <input type="text" id="no-comments" />
+              <p>Please do not include personal contact details.</p>
+            </div>
+          </FieldsetWrapper>
+        )}
+        {this.state.yesFieldsetDisplay && (
+          <FieldsetWrapper ariaExpanded={this.state.noFieldsetDisplay}>
+            <legend>Yes fieldset</legend>
+          </FieldsetWrapper>
+        )}
+      </form>
     );
   }
 }
