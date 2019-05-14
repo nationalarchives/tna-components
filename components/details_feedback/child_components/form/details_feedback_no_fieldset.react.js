cc5118a2beb132bb44fd1a388757c90c696d468b
Punal Chotrani <punalsc@gmail.com>
2019-05-14 10:18:57 +0100

    Refactored code requested by reviewer.

--- components/details_feedback/child_components/form/details_feedback_no_fieldset.react.js
+++ components/details_feedback/child_components/form/details_feedback_no_fieldset.react.js
@@ -1,31 +1,22 @@
 import React, { Component } from 'react';
 //import FeedbackButton from "../button/details_feedback_widget_button.react";
 
-class NoFieldset extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      displayFieldSet: true
-    };
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        {this.state.displayFieldSet && (
-          <fieldset className="noBorder" id="no-fieldset">
-            <legend>We'd like to hear from you</legend>
-            <label for="explore-comments">
-              Your feedback helps us improve our services. Please share any
-              comments below.
-            </label>
-            <input type="text" id="no-comments" />
-            <p>Please do not include personal contact details.</p>
-          </fieldset>
-        )}
-      </React.Fragment>
-    );
-  }
-}
+const NoFieldset = ({ displayFieldSet }) => {
+  return (
+    <React.Fragment>
+      {displayFieldSet && (
+        <fieldset id="no-fieldset">
+          <legend>We'd like to hear from you</legend>
+          <label for="explore-comments">
+            Your feedback helps us improve our services. Please share any
+            comments below.
+          </label>
+          <input type="text" id="no-comments" />
+          <p>Please do not include personal contact details.</p>
+        </fieldset>
+      )}
+    </React.Fragment>
+  );
+};
 
 export default NoFieldset;
