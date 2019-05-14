c79403d3c2d656b060155cb7ba3fdee76e848e96
Punal Chotrani <punalsc@gmail.com>
2019-05-14 12:43:03 +0100

    Refactored button component based based on reviewer suggestion

--- components/details_feedback/child_components/button/details_feedback_widget_button.react.js
+++ components/details_feedback/child_components/button/details_feedback_widget_button.react.js
@@ -2,20 +2,13 @@ import React, { Component } from 'react';
 
 class FeedbackButton extends Component {
   render() {
-    const { title, className } = this.props;
+    const { buttonText } = this.props;
     return (
       <React.Fragment>
-        <button className={className} value={title}>
-          {title}
-        </button>
+        <button>{buttonText}</button>
       </React.Fragment>
     );
   }
 }
-
-FeedbackButton.defaultProps = {
-  title: 'Enter button title',
-  className: null
-};
 
 export default FeedbackButton;
