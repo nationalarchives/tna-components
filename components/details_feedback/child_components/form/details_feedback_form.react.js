49a0cf4b2a54677fbbe6de168ca3cd8731866958
Punal Chotrani <punalsc@gmail.com>
2019-05-17 16:42:25 +0100

    Destructuring and adding state into a seperate data file

--- components/details_feedback/child_components/form/details_feedback_form.react.js
+++ components/details_feedback/child_components/form/details_feedback_form.react.js
@@ -3,14 +3,10 @@ import Fieldset from '../fieldset/details_feedback_fieldset.react';
 import Button from '../button/details_feedback_widget_button.react';
 import Checkbox from '../checkbox/details_feedback_widget_checkbox.react';
 import Comment from '../comment/details_feedback_comment.react';
-//import data from '../../data';
+import { Data } from '../../data';
 
 class DetailsFeedbackForm extends Component {
-  state = {
-    initialQuestion: true,
-    noFieldsetDisplay: false,
-    yesFieldsetDisplay: false
-  };
+  state = Data;
 
   cancelObj = () => {
     return {
@@ -49,10 +45,16 @@ class DetailsFeedbackForm extends Component {
   };
 
   render() {
+    const {
+      initialQuestion,
+      noFieldsetDisplay,
+      yesFieldsetDisplay,
+      checkboxtData
+    } = this.state;
     return (
       <div>
         <form>
-          {this.state.initialQuestion && (
+          {initialQuestion && (
             <Fieldset
               legendText="Could this page be improved?"
               className="initail-question">
@@ -69,7 +71,7 @@ class DetailsFeedbackForm extends Component {
             </Fieldset>
           )}
 
-          {this.state.noFieldsetDisplay && (
+          {noFieldsetDisplay && (
             <Fieldset
               legendText="We'd like to hear from you"
               className="noMarginLeft fadeIn">
@@ -86,35 +88,18 @@ class DetailsFeedbackForm extends Component {
               />
             </Fieldset>
           )}
-          {this.state.yesFieldsetDisplay && (
+          {yesFieldsetDisplay && (
             <Fieldset
               legendText="Please let us know why you are dissatisfied"
               className="fadeIn">
-              <Checkbox
-                id="did-not-understand"
-                labelText="I did not understand the information on the page"
-              />
-              <Checkbox
-                id="too-much-information"
-                labelText="There was too much information on the page"
-              />
-              <Checkbox
-                id="expected-the-record"
-                labelText="I expected to see the record itself, not just a description"
-              />
-              <Checkbox
-                id="access-the-record"
-                labelText="I did not understand how to access the record"
-              />
-              <Checkbox
-                id="did-not-know-was-tna"
-                labelText="I did not realise I was looking at The National Archives' UK website"
-              />
-              <Checkbox
-                id="not-enough-information"
-                labelText="There was not enough information"
-              />
-              <Checkbox id="other" labelText="Other" />
+              {checkboxtData.map((eachItem, index) => (
+                <Checkbox
+                  key={index}
+                  id={eachItem.id}
+                  labelText={eachItem.labelText}
+                />
+              ))}
+
               <Comment
                 id="comment-for-dissatisfaction"
                 commentText="Your feedback helps us improve our services. Please share any comments below."
