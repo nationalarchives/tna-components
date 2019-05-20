f5cf16f8c9b45374fd1a9530c2d4600b55b8897f
AshTNA <ashleyaburnett@gmail.com>
2019-05-20 16:44:06 +0100

    Add prototype functionality for show/hide on mobile

--- components/global_search/global-search.react.js
+++ components/global_search/global-search.react.js
@@ -11,12 +11,15 @@ class GlobalSearch extends Component {
         this.toggle_search_options = this.toggle_search_options.bind(this);
         this.search_bar_focused = this.search_bar_focused.bind(this);
         this.get_select_search_type = this.get_select_search_type.bind(this);
+        this.show_hide_mobile = this.show_hide_mobile.bind(this);
 
-        this.checkbox_ref = React.createRef();
+        this.show_search_options_ref = React.createRef();
+        this.show_hide_mobile_ref = React.createRef();
 
         this.state = {
 
             show_search_options: true,
+            show_mobile_search: false,
 
             active_search: {}, // This is assigned to the first option below
 
@@ -63,7 +66,7 @@ class GlobalSearch extends Component {
     }
 
     search_bar_focused() {
-        this.checkbox_ref.current.checked = false;
+        this.show_search_options_ref.current.checked = false;
         this.setState({show_search_options: false});
     }
 
@@ -78,43 +81,62 @@ class GlobalSearch extends Component {
         )
     }
 
+    show_hide_mobile(){
+        this.setState({show_mobile_search: !this.state.show_mobile_search});
+    }
+
     render() {
         return (
-            <form aria-labelledby="global_search_label" role="search" className="global-search-js"
-                  action={this.state.active_search.url}>
+            <div>
 
-
-                {(this.props.desktop) ?
-                    <fieldset id="search-options">
-                        <legend className="sr-only">{this.state.search_selector.label}</legend>
-                        <input type="checkbox" id={this.state.search_selector.id}
-                               aria-label={this.state.search_selector.label} className="sr-only"
-                               onChange={this.toggle_search_options}
-                               ref={this.checkbox_ref}/>
-                        <label htmlFor={this.state.search_selector.id} className="show-search-options">
-                            <span className="sr-only"> {this.state.search_selector.label}</span>
+                {(!this.props.desktop) ?
+                    <fieldset id="mobile-search-options">
+                        <legend className="sr-only">Show search box</legend>
+                        <input type="checkbox" id='show-hide-mobile'
+                               aria-label='show-hide-mobile' className="sr-only"
+                               onChange={this.show_hide_mobile}
+                               ref={this.show_hide_mobile_ref}/>
+                        <label htmlFor='show-hide-mobile' className="show-hide-mobile">
+                            <span className="sr-only"> Show search box</span>
                         </label>
-                    </fieldset> : null}
+                    </fieldset>
+                     : null }
+
+                <form aria-labelledby="global_search_label" role="search" aria-hidden={(!this.props.desktop && !this.state.show_mobile_search)}
+                      action={this.state.active_search.url}>
+
+                    {(this.props.desktop) ?
+                        <fieldset id="search-options">
+                            <legend className="sr-only">{this.state.search_selector.label}</legend>
+                            <input type="checkbox" id={this.state.search_selector.id}
+                                   aria-label={this.state.search_selector.label} className="sr-only"
+                                   onChange={this.toggle_search_options}
+                                   ref={this.show_search_options_ref}/>
+                            <label htmlFor={this.state.search_selector.id} className="show-search-options">
+                                <span className="sr-only"> {this.state.search_selector.label}</span>
+                            </label>
+                        </fieldset> : null}
 
 
-                {(this.state.show_search_options === true && this.props.desktop) ?
-                    this.get_select_search_type() : null
-                }
+                    {(this.state.show_search_options === true && this.props.desktop) ?
+                        this.get_select_search_type() : null
+                    }
 
-                <fieldset id="search-query">
-                    <legend className="sr-only">{this.state.search_query_legend}</legend>
-                    <input type="search" autoComplete="off" role="search" name="_q"
-                           aria-label={this.state.active_search.label}
-                           placeholder={this.state.active_search.label}
-                           className='focusable-outline'
-                           onFocus={this.search_bar_focused}/>
-                    <input type="submit" className='search-submit focusable-outline'/>
-                </fieldset>
+                    <fieldset id="search-query">
+                        <legend className="sr-only">{this.state.search_query_legend}</legend>
+                        <input type="search" autoComplete="off" role="search" name="_q"
+                               aria-label={this.state.active_search.label}
+                               placeholder={this.state.active_search.label}
+                               className='focusable-outline'
+                               onFocus={this.search_bar_focused}/>
+                        <input type="submit" className='search-submit focusable-outline'/>
+                    </fieldset>
 
-                {(this.state.show_search_options === true && !this.props.desktop) ?
-                    this.get_select_search_type() : null
-                }
-            </form>
+                    {(this.state.show_search_options === true && !this.props.desktop) ?
+                        this.get_select_search_type() : null
+                    }
+                </form>
+            </div>
         );
     }
 }
