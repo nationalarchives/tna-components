a01aa8a3407778ed695f9a55d9a3353ebadf9f43
Punal Chotrani <punalsc@gmail.com>
2019-05-28 12:38:58 +0100

    Appended conditions for the runner

--- src/runners/details-feedback.js
+++ src/runners/details-feedback.js
@@ -3,27 +3,34 @@ import ReactDOM from 'react-dom';
 import DetailsFeedbackWidget from '../../components/details_feedback/details_feedback_widget.react';
 
 {
-  const detailsPage = document.querySelector('.details-page');
-  const path = window.location.pathname;
-  const regex = /\/details\/r\/(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/;
+  const detailsPage = document.querySelector('.details-page'),
+    path = window.location.pathname,
+    regex = /\/details\/r\/(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/,
+    regexDev = /http:\/\/localhost:3000\/details/r/i,
+    regexTest = /https:\/\/test.discovery.nationalarchives.gov.uk/i,
+    regexLive = /https:\/\/discovery.nationalarchives.gov.uk/i,
+    regexDevLive = /http:\/\/dev.discovery.nationalarchives.gov.uk/i,
+    regexDevDiscovery = /http:\/\/localhost:81/i;
 
   if (detailsPage && path.match(regex)) {
     if (detailsPage.contains(document.getElementById('page_wrap'))) {
       const pageWrap = document.getElementById('page_wrap');
-      if (!document.getElementById('details-feedback-wrapper')) {
-        let asideDom = `
-					<aside id="feedback-widget-row">
-						<div>
-              <div id="details-feedback-wrapper"></div>
-						</div>
-					</aside>`;
-
-        pageWrap.insertAdjacentHTML('beforeend', asideDom);
-
-        ReactDOM.render(
-          <DetailsFeedbackWidget />,
-          document.getElementById('details-feedback-wrapper')
-        );
+      if(path.match(regexDev) || path.regexDevDiscovery || path.match(regexTest) || path.match(regexLive) || path.match(regexDevLive) ||path.match(regexDevDiscovery)) {
+        if (!document.getElementById('details-feedback-wrapper')) {
+           const asideDom = `
+            <aside id="feedback-widget-row">
+              <div>
+                <div id="details-feedback-wrapper"></div>
+              </div>
+            </aside>`;
+  
+          pageWrap.insertAdjacentHTML('beforeend', asideDom);
+  
+          ReactDOM.render(
+            <DetailsFeedbackWidget />,
+            document.getElementById('details-feedback-wrapper')
+          );
+        }
       }
     }
   }
