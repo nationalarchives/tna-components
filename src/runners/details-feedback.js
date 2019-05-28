a85e27e3b9553f9bc91d009a37866a534ec8fbd0
Punal Chotrani <punalsc@gmail.com>
2019-05-28 17:57:20 +0100

    Removed Regex for dev environment from runner

--- src/runners/details-feedback.js
+++ src/runners/details-feedback.js
@@ -6,7 +6,6 @@ import DetailsFeedbackWidget from '../../components/details_feedback/details_fee
   const detailsPage = document.querySelector('.details-page'),
     path = window.location.pathname,
     regex = /\/details\/r\/(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/,
-    regexDev = /http:\/\/localhost:3000\/details/r/i,
     regexTest = /https:\/\/test.discovery.nationalarchives.gov.uk/i,
     regexLive = /https:\/\/discovery.nationalarchives.gov.uk/i,
     regexDevLive = /http:\/\/dev.discovery.nationalarchives.gov.uk/i,
@@ -15,17 +14,23 @@ import DetailsFeedbackWidget from '../../components/details_feedback/details_fee
   if (detailsPage && path.match(regex)) {
     if (detailsPage.contains(document.getElementById('page_wrap'))) {
       const pageWrap = document.getElementById('page_wrap');
-      if(path.match(regexDev) || path.regexDevDiscovery || path.match(regexTest) || path.match(regexLive) || path.match(regexDevLive) ||path.match(regexDevDiscovery)) {
+      if (
+        path.regexDevDiscovery ||
+        path.match(regexTest) ||
+        path.match(regexLive) ||
+        path.match(regexDevLive) ||
+        path.match(regexDevDiscovery)
+      ) {
         if (!document.getElementById('details-feedback-wrapper')) {
-           const asideDom = `
+          const asideDom = `
             <aside id="feedback-widget-row">
               <div>
                 <div id="details-feedback-wrapper"></div>
               </div>
             </aside>`;
-  
+
           pageWrap.insertAdjacentHTML('beforeend', asideDom);
-  
+
           ReactDOM.render(
             <DetailsFeedbackWidget />,
             document.getElementById('details-feedback-wrapper')
