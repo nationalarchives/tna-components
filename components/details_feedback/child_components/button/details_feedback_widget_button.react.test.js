791d7462ee4f1abc0a0c520d0b7ac15cfbf42378
Punal Chotrani <punalsc@gmail.com>
2019-05-30 15:34:31 +0100

    Updating tests

--- components/details_feedback/child_components/button/details_feedback_widget_button.react.test.js
+++ components/details_feedback/child_components/button/details_feedback_widget_button.react.test.js
@@ -3,7 +3,7 @@ import TestRenderer from 'react-test-renderer';
 import renderer from 'react-test-renderer';
 import Button from './details_feedback_widget_button.react';
 
-let testRenderer, testInstance, testRendererToJson;
+let testRenderer, testRendererToJson;
 
 // Snapshot test 1
 it('renders the button correctly', () => {
@@ -13,11 +13,7 @@ it('renders the button correctly', () => {
 
 // Snapshot test 2
 it('renders correctly when there are multiple items', () => {
-  const items = [
-    'Hello Button',
-    () => console.log('Button OnClick CB'),
-    'submit'
-  ];
+  const items = ['Hello Button', () => console.log('Button OnClick'), 'submit'];
   const tree = renderer
     .create(<Button buttonText={items[0]} onClick={items[1]} type={items[2]} />)
     .toJSON();
@@ -33,25 +29,10 @@ describe("Check DOM element and it's attributes", () => {
       onClick={() => console.log('This is a button component')}
     />
   );
-  testInstance = testRenderer.root;
-  testRendererToJson = testRenderer.toJSON();
 
+  testRendererToJson = testRenderer.toJSON();
   it('Should have an <button> tag', () => {
     expect(testRendererToJson.type).toBe('button');
     expect(testRendererToJson.type === 'div').toBeFalsy;
-  });
-});
-
-describe('<Button>', () => {
-  it('renders the correct text', () => {
-    const buttonText = 'submit';
-    const inst = renderer.create(<Button buttonText={buttonText} />);
-  });
-
-  it('renders the correct type', () => {
-    const type = 'submit';
-    const inst = renderer.create(
-      <Button buttonText="buttonText" type={type} />
-    );
   });
 });
