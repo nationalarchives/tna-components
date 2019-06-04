5ab725e48b3af99407fc6e3c18b214a2fe2348a2
Punal Chotrani <punalsc@gmail.com>
2019-06-04 15:50:05 +0100

    Snapshot and unit testing for feedback form

--- components/details_feedback/child_components/form/details_feedback_form.react.test.js
+++ components/details_feedback/child_components/form/details_feedback_form.react.test.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import DetailsFeedbackForm from './details_feedback_form.react';
+import { create } from 'react-test-renderer';
 import renderer from 'react-test-renderer';
+// import Button from '../button/details_feedback_widget_button.react';
 
-// // Snapshot 1
+// Snapshot 1
 it('renders correctly', () => {
   const tree = renderer.create(<DetailsFeedbackForm />).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+// Unit test
+describe('Test state', () => {
+  const component = create(<DetailsFeedbackForm />);
+  const instance = component.getInstance();
+  const {
+    initialQuestion,
+    noFieldsetDisplay,
+    yesFieldsetDisplay,
+    message,
+    displayForm,
+    gtmCheckboxes,
+    nothingToImproveEventLabel
+  } = instance.state;
+  it('checking the initial state', () => {
+    expect(initialQuestion).toBeTruthy();
+    expect(noFieldsetDisplay).toBeFalsy();
+    expect(yesFieldsetDisplay).toBeFalsy();
+    expect(message).toBeFalsy();
+    expect(displayForm).toBeTruthy();
+    expect(gtmCheckboxes).toEqual([]);
+    expect(nothingToImproveEventLabel).toBe('No comments made');
+  });
 });
