b1d0775c7932cef6284209186cd24b211c61bc01
JamesWChan <jwchan1993@gmail.com>
2019-06-18 13:37:11 +0100

    Create WTEG feedback form

---
+++ components/feedback/child_components/form/wteg_feedback_form/wteg_feedback_form.react.js
@@ -0,0 +1,142 @@
+import React, {Component} from 'react';
+import Button from '../../button/details_feedback_widget_button.react';
+import Fieldset from '../../fieldset/details_feedback_fieldset.react';
+import Comment from "../../comment/details_feedback_comment.react";
+import { Data } from '../../../data';
+import Message from "../../message/details_feedback_message.react";
+
+class WTEGFeedbackForm extends Component{
+	constructor(props){
+		super(props);
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.toggleFieldset = this.toggleFieldset.bind(this);
+		this.state = {
+			comments: '',
+			Data,
+			displayCommentBox: false,
+			displayForm: true,
+			guideHelpful: false,
+			initialQuestion: true,
+			message: false
+		}
+	}
+
+	handleChange(e) {
+		let name = e.target.name;
+		let value = e.target.value;
+		this.setState({
+			[name]: value
+		});
+	}
+
+	handleSubmit(e) {
+		let comments = this.state.comments;
+		if(!comments.length > 0){
+			comments = 'No comments made.';
+		}
+
+		this.setState({ message: true });
+		this.setState({ displayCommentBox: false });
+		this.setState({ displayForm: false });
+		//Push to Data Layer
+		e.preventDefault();
+	}
+
+	toggleFieldset = (objKeyOne, boolOne, objKeyTwo, boolTwo, objKeyThree, boolThree) => {
+		this.setState({ [objKeyOne]: boolOne });
+		this.setState({ [objKeyTwo]: boolTwo });
+		this.setState({ [objKeyThree]: boolThree });
+	};
+
+	render(){
+		const {
+			displayCommentBox,
+			displayForm,
+			initialQuestion,
+			guideHelpful,
+			message
+		} = this.state;
+
+		const { yesFieldsetData, noFieldsetData, messageText } = this.state.Data;
+
+		return(
+			<div>
+				{displayForm && (
+					<form onSubmit={this.handleSubmit}>
+						{initialQuestion && (
+							<Fieldset
+								legendText="Did you find this helpful?"
+								className="initial-question">
+								<Button
+									buttonText="Yes"
+									onClick={() => {
+										this.toggleFieldset(
+											'displayCommentBox',
+											true,
+											'initialQuestion',
+											false,
+											'guideHelpful',
+											true
+										)
+									}}
+									type="button"
+								/>
+								<Button
+									buttonText="No"
+									onClick={() => {
+										this.toggleFieldset(
+											'displayCommentBox',
+											true,
+											'initialQuestion',
+											false,
+											'guideHelpful',
+											false
+										)
+									}}
+									type="button"
+								/>
+							</Fieldset>
+						)}
+						{displayCommentBox && (
+							<Fieldset
+								id="no_fieldset"
+								legendText={guideHelpful ? noFieldsetData.legend : yesFieldsetData.legend}
+								className="noMarginLeft fadeIn">
+								<Comment
+									id="comments"
+									commentLabel={guideHelpful ?
+										noFieldsetData.commentData.commentLabel : yesFieldsetData.commentData.commentLabel}
+									commentWarning={guideHelpful ?
+										noFieldsetData.commentData.commentWarning : yesFieldsetData.commentData.commentWarning}
+									onChange={this.handleChange}
+									autoFocus="true"
+								/>
+								<Button buttonText="Send" type="submit" />
+								<Button
+									buttonText="Cancel"
+									type="reset"
+									onClick={() => {
+										this.toggleFieldset(
+											'displayCommentBox',
+											false,
+											'initialQuestion',
+											true,
+											'guideHelpful',
+											false
+										)
+									}}
+								/>
+							</Fieldset>
+						)}
+					</form>
+				)}
+				{message && (
+					<Message className="fadeIn" message={messageText.thankyou} />
+				)}
+			</div>
+		)
+	}
+}
+
+export default WTEGFeedbackForm
--- components/feedback/wteg_feedback_widget.react.js
+++ components/feedback/wteg_feedback_widget.react.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import WTEGFeedbackForm from './child_components/form/wteg_feedback_form/wteg_feedback_form.react';
 
-import '../wteg_feedback/style.scss';
+import './style.scss';
 
 class WTEGFeedbackWidget extends Component {
 	render() {
 		return (
 			<React.Fragment>
-				<h2>Did you find this helpful?</h2>
+				<h2>Feedback</h2>
+				<WTEGFeedbackForm />
 			</React.Fragment>
 		);
 	}
