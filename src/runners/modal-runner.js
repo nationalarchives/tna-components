bf33f40c7c311839ed5349664a84933cca43e945
Punal Chotrani <punalsc@gmail.com>
2019-06-27 18:31:29 +0100

    Added screen to runner

--- src/runners/modal-runner.js
+++ src/runners/modal-runner.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import WTEGModal from '../../components/modal/wtegModel/wtegModal.react';
 {
-  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent),
-    detailsPage = document.querySelector('.details-page');
-
-  if (!isMobile) {
-    if (detailsPage.contains(document.getElementById('wteg-image-wrapper'))) {
-      if (document.getElementById('wteg-image-wrapper')) {
+  if (window.innerWidth >= 768) {
+    if (document.querySelector('.details-page')) {
+      const detailsPage = document.querySelector('.details-page');
+      if (detailsPage.contains(document.getElementById('wteg-image-wrapper'))) {
         const wtegWrapper = document.getElementById('wteg-image-wrapper');
         if (wtegWrapper.querySelector('img.what-to-expect-image')) {
           const wtegImg = wtegWrapper.querySelector('img.what-to-expect-image');
