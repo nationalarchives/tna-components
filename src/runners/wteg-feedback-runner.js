ef20cd2ee4dd2f88471c1b3882583acb24926451
JamesWChan <jwchan1993@gmail.com>
2019-06-28 11:54:44 +0100

    WTEG runner fixes

--- src/runners/wteg-feedback-runner.js
+++ src/runners/wteg-feedback-runner.js
@@ -2,19 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import WTEGFeedbackWidget from '../../components/feedback/wteg_feedback_widget.react';
 
-if(document.querySelector('.details-page')){
+if (document.querySelector('.details-page')) {
     const detailsPage = document.querySelector('.details-page');
-    if(detailsPage.contains(document.getElementById('what-to-expect'))){
-        if(!document.getElementById('what-to-expect-form')){
-            const wtegWrapper = document.getElementById('what-to-expect');
-            const wtegWrapperId = 'what-to-expect-form';
-            const wtegFeedbackDiv = `<div id=${wtegWrapperId}></div>`;
-            wtegWrapper.insertAdjacentHTML('beforeend', wtegFeedbackDiv);
+    if (detailsPage.contains(document.getElementById('what-to-expect'))){
+    const wteID =  document.getElementById('what-to-expect');
+    console.log(wteID);
 
-            ReactDOM.render(
-                <WTEGFeedbackWidget />,
-                document.getElementById(wtegWrapperId)
-            );
-        }
+    if (!document.getElementById('what-to-expect-form')){
+        const wtegFormID = 'what-to-expect-form';
+        const wtegDOM = `<div id=${wtegFormID}></div>`;
+
+        wteID.insertAdjacentHTML('beforeend', wtegDOM);
+
+        ReactDOM.render(
+            <WTEGFeedbackWidget />,
+            document.getElementById(wtegFormID)
+         );
+
     }
+  }
 }
