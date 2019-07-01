d2bb36f70306066d159c97cc254299b379621f5c
Punal Chotrani <punalsc@gmail.com>
2019-07-01 12:10:59 +0100

    Updated runner for wteg feedback

--- src/runners/wteg-feedback-runner.js
+++ src/runners/wteg-feedback-runner.js
@@ -2,21 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import WTEGFeedbackWidget from '../../components/feedback/wteg_feedback_widget.react';
 
-if (document.querySelector('.details-page')) {
-  const detailsPage = document.querySelector('.details-page');
-  if (detailsPage.contains(document.getElementById('what-to-expect'))) {
-    const wteID = document.getElementById('what-to-expect');
+{
+  const url = window.location.href,
+    devEnv = 'http://localhost:3000/what-to-expect-guide',
+    regexTest = /https:\/\/test.discovery.nationalarchives.gov.uk\/details\/r/i,
+    regexLive = /https:\/\/discovery.nationalarchives.gov.uk\/details\/r/i,
+    regexDevLive = /http:\/\/dev.discovery.nationalarchives.gov.uk\/details\/r/i,
+    regexDevDiscovery = /http:\/\/localhost:81\/details\/r/i;
+  if (
+    url.match(devEnv) ||
+    url.match(regexTest) ||
+    url.match(regexDevDiscovery) ||
+    url.match(regexDevLive) ||
+    url.match(regexLive)
+  ) {
+    if (document.querySelector('.details-page')) {
+      const detailsPage = document.querySelector('.details-page');
+      if (detailsPage.contains(document.getElementById('what-to-expect'))) {
+        const wteID = document.getElementById('what-to-expect');
 
-    if (!document.getElementById('what-to-expect-form')) {
-      const wtegFormID = 'what-to-expect-form';
-      const wtegDOM = `<div id=${wtegFormID}></div>`;
+        if (!document.getElementById('what-to-expect-form')) {
+          const wtegFormID = 'what-to-expect-form';
+          const wtegDOM = `<div id=${wtegFormID}></div>`;
 
-      wteID.insertAdjacentHTML('beforeend', wtegDOM);
+          wteID.insertAdjacentHTML('beforeend', wtegDOM);
 
-      ReactDOM.render(
-        <WTEGFeedbackWidget />,
-        document.getElementById(wtegFormID)
-      );
+          ReactDOM.render(
+            <WTEGFeedbackWidget />,
+            document.getElementById(wtegFormID)
+          );
+        }
+      }
     }
   }
 }
