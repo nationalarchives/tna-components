e3d3635bbbb81e9887bd3e5523b36f486eb1524a
NetDesignr <michaiel.webdesigner@gmail.com>
2019-10-09 16:56:41 +0100

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,43 +4,52 @@ const discovery_version = `1.1.8`;
 const website_version = `1.0.1`;
 
 module.exports = {
-	entry: {
-		[`discovery-${discovery_version}`]: './src/discovery.js',
-		[`website-${website_version}`]: './src/website.js'
-	},
-	mode: 'production',
-	output: {
-		filename: '[name].js',
-		path: path.resolve(__dirname, 'dist')
-	},
-	module: {
-		rules: [
-			{
-				test: /\.m?js$/,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: [
-							'@babel/preset-env',
-							'@babel/preset-react',
-							{
-								plugins: ['@babel/plugin-proposal-class-properties']
-							}
-						]
-					}
-				}
-			},
-			{
-				test: /\.scss$/,
-				use: [
-					'style-loader', // creates style nodes from JS strings
-					'css-loader', // translates CSS into CommonJS
-					'sass-loader', // compiles Sass to CSS, using Node Sass by default
-					'file-loader',
-					'url-loader'
-				]
-			}
-		]
-	}
+  entry: {
+    [`discovery-${discovery_version}`]: './src/discovery.js',
+    [`website-${website_version}`]: './src/website.js'
+  },
+  mode: 'production',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              {
+                plugins: ['@babel/plugin-proposal-class-properties']
+              }
+            ]
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', // creates style nodes from JS strings
+          'css-loader', // translates CSS into CommonJS
+          'sass-loader' // compiles Sass to CSS, using Node Sass by default
+        ]
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        issuer: {
+          test: /\.jsx?$/
+        },
+        use: ['babel-loader', '@svgr/webpack', 'url-loader']
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader'
+      }
+    ]
+  }
 };
