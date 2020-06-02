78767b4526a07799b3a6b89fd65745bbae3833a1
Gwyn Jones <m@gwnjns.com>
2020-06-02 11:36:58 +0100

    Add some comments to the component
    This component makes heavy use of React hooks which are not intuitive
    to those who are not familiar with how they work. These comments are
    intended to help future developers get past some of the conceptual
    humps that hooks introduce.

--- components/global_search/global-search.react.js
+++ components/global_search/global-search.react.js
@@ -6,18 +6,27 @@ import "./styles.scss";
 function GlobalSearch(props) {
     const environment = props.desktop ? "desktop" : "mobile";
 
+    // These destructuring assignments will result in a constant name and setter
     const [formAction, setFormAction] = useState(FormData.options[0].action);
     const [check, setCheck] = useState(false);
     const [radio, setRadio] = useState(FormData.options[0].id + "-" + environment);
     const [placeholder, setPlaceholder] = useState(FormData.options[0].label);
     const [formData, setFormData] = useState(FormData);
 
-
+    // Provides us with a reference to the search input
     const inputSearchRef = useRef();
+
+    // useFocus is a custom hook which manages 'focus' and 'blur'
+    // on the given ref, which is the search input
     const inputFocused = useFocus(inputSearchRef);
 
     const checkBoxChecked = () => setCheck(!check);
 
+    // useEffect is used to perform side effects
+    // in this case it sets the check constant to false.
+    // By default, useEffect runs after every render but
+    // here the a second argument is passed - inputFocussed
+    // which means the hook will run in response to inputFocussed
     useEffect(() => {
         inputFocused && setCheck(false);
     }, [inputFocused]);
@@ -68,7 +77,7 @@ function GlobalSearch(props) {
                                                         setRadio(data.id + "-" + environment);
                                                     }}
                                                     checked={(data.id + "-" + environment) == radio}
-                        
+
                                                     className={props.desktop && "sr-only"}
                                                 />
                                                 <label htmlFor={data.id  + "-" + environment}>{data.label}</label>
@@ -94,7 +103,7 @@ function GlobalSearch(props) {
                         </label>
                         {!props.desktop ? (<input type="submit" value={formData.labels.mobile_search_text}/>)
                         : (<input type="submit" />)}
-                        
+
                     </fieldset>
                 </form>
             </div>
