e783e913a3040224517bec649ac34b71d24ebab3
AshTNA <ashleyaburnett@gmail.com>
2021-09-15 11:27:47 +0100

    Add seperate runner for Discovery global search
    This allow 'Search for records' to be the first search option. It also fixes React crashing on certain Discovery pages such as the homepage, as Discovery does not show global search across the application

--- src/runners/global-search-runner.js
+++ src/runners/global-search-runner.js
@@ -4,12 +4,28 @@ import GlobalSearch from '../../components/global_search/global-search.react';
 
 // import component
 
-ReactDOM.render(
-	<GlobalSearch desktop={true}/>,
-	document.getElementById('global-search-desktop-component'),
-);
 
-ReactDOM.render(
-	<GlobalSearch desktop={false}/>,
-	document.getElementById('global-search-mobile-component'),
-);
+function renderIfExists(id, desktop, defaultFormDataIndex) {
+
+	if(document.getElementById(id)) {
+		ReactDOM.render(
+			<GlobalSearch desktop={desktop} defaultFormDataIndex={defaultFormDataIndex}
+			/>,
+			document.getElementById(id)
+		)
+	}
+
+
+}
+
+
+// Non-discovery runner
+
+renderIfExists("global-search-desktop-component", true, 0);
+renderIfExists("global-search-mobile-component", false, 0);
+
+
+// Discovery runner
+
+renderIfExists("global-search-desktop-component-discovery", true, 1);
+renderIfExists("global-search-mobile-component-discovery", false, 1);
